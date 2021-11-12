5b21b3d290d46f0aecde9fc3f61239bfb1b0657b
yuruic <93725398+yuruic@users.noreply.github.com>
2021-11-12 12:43:28 -0500

    Add file for "fan-out" tree

---
+++ pandas_notes/pd_topic_group0_4b_yuruic.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.5
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+import pandas as pd
+import numpy as np
+
+# ## Topics in Pandas
+# **Stats 507, Fall 2021** 
+#   
+
+# ## Contents
+# Add a bullet for each topic and link to the level 2 title header using 
+# the exact title with spaces replaced by a dash. 
+#
+# + [Topic Title](#Topic-Title)
+# + [Topic 2 Title](#Topic-2-Title)
+
+# ## Topic Title
+# Include a title slide with a short title for your content.
+# Write your name in *bold* on your title slide. 
+
+#
+
+# ## Contents
+# + [Processing Time Data](#Processing-Time-Data)
+# + [Topic 2 Title](#Topic-2-Title)
+
+# * ###  Processing Time Data
+#
+# **Yurui Chang**
+
+# #### Pandas.to_timedelta
+#
+# - To convert a recognized timedelta format / value into a Timedelta type
+# - the unit of the arg
+#   * 'W'
+#   * 'D'/'days'/'day'
+#   * ‘hours’ / ‘hour’ / ‘hr’ / ‘h’
+#   * ‘m’ / ‘minute’ / ‘min’ / ‘minutes’ / ‘T’
+#   * ‘S’ / ‘seconds’ / ‘sec’ / ‘second’
+#   * ‘ms’ / ‘milliseconds’ / ‘millisecond’ / ‘milli’ / ‘millis’ / ‘L’
+#   * ‘us’ / ‘microseconds’ / ‘microsecond’ / ‘micro’ / ‘micros’ / ‘U’
+#   * ‘ns’ / ‘nanoseconds’ / ‘nano’ / ‘nanos’ / ‘nanosecond’ / ‘N’
+
+# * Parsing a single string to a Timedelta
+# * Parsing a list or array of strings
+# * Converting numbers by specifying the unit keyword argument
+
+time1 = pd.to_timedelta('1 days 06:05:01.00003')
+time2 = pd.to_timedelta('15.5s')
+print([time1, time2])
+pd.to_timedelta(['1 days 06:05:01.00003', '15.5s', 'nan'])
+
+pd.to_timedelta(np.arange(5), unit='d')
+
+# #### pandas.to_datetime
+#
+# * To convert argument to datetime
+# * Returns: datetime, return type dependending on input
+#   * list-like: DatetimeIndex
+#   * Series: Series of datetime64 dtype
+#   * scalar: Timestamp
+# * Assembling a datetime from multiple columns of a DataFrame
+# * Converting Pandas Series to datetime w/ custom format
+# * Converting Unix integer (days) to datetime
+# * Convert integer (seconds) to datetime
+
+s = pd.Series(['date is 01199002',
+           'date is 02199015',
+           'date is 03199020',
+           'date is 09199204'])
+pd.to_datetime(s, format="date is %m%Y%d")
+
+time1 = pd.to_datetime(14554, unit='D', origin='unix')
+print(time1)
+time2 = pd.to_datetime(1600355888, unit='s', origin='unix')
+print(time2)
+
+
